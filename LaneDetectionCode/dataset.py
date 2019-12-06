8e69fc224ce36ee53c601ef124b28328a28184ec
Luyu Liu <liuluyu0378@gmail.com>
2019-12-06 14:22:19 -0500

    Update dataset.py

--- LaneDetectionCode/dataset.py
+++ LaneDetectionCode/dataset.py
@@ -42,7 +42,7 @@ class RoadSequenceDatasetList(Dataset):
 
     def __init__(self, file_path, transforms):
 
-        self.img_list = readTxt(file_path)
+        self.img_list = glob.glob(file_path + "/*.jpg")
         self.dataset_size = len(self.img_list)
         self.transforms = transforms
     def __len__(self):
